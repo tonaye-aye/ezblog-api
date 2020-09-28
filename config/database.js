306e195615a0f40d57fb8c99622ee717f6511ff9
Tony Gorgioski <tony.gorgioski@gmail.com>
2020-09-29 09:25:43 +1000

    first commit

---
+++ config/database.js
@@ -0,0 +1,34 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         srv: env.bool('DATABASE_SRV', false),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME', 'blog'),
+//         username: env('DATABASE_USERNAME', ''),
+//         password: env('DATABASE_PASSWORD', ''),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// });

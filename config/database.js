0c1d2003f6e6c983755e726668229b4158ab4f9e
Tony Gorgioski <tony.gorgioski@gmail.com>
2020-09-29 10:15:35 +1000

    Added category; design

--- config/database.js
+++ config/database.js
@@ -15,23 +15,23 @@ module.exports = ({ env }) => ({
   },
 });
 
-// Dev Database
+//Dev Database
 // module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
+//   defaultConnection: "default",
 //   connections: {
 //     default: {
-//       connector: 'mongoose',
+//       connector: "mongoose",
 //       settings: {
-//         host: env('DATABASE_HOST', '127.0.0.1'),
-//         srv: env.bool('DATABASE_SRV', false),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'blog'),
-//         username: env('DATABASE_USERNAME', ''),
-//         password: env('DATABASE_PASSWORD', ''),
+//         host: env("DATABASE_HOST", "127.0.0.1"),
+//         srv: env.bool("DATABASE_SRV", false),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", "blog"),
+//         username: env("DATABASE_USERNAME", ""),
+//         password: env("DATABASE_PASSWORD", ""),
 //       },
 //       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', false),
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", false),
 //       },
 //     },
 //   },
